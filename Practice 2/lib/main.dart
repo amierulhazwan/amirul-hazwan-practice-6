ec0a90220776e12e7a6defc40598c57374194667
Amirul Hazwan <amirulhazwan1997@gmail.com>
2021-11-02 20:08:35 +0800

    Practice 2 Update

--- Practice 2/lib/main.dart
+++ Practice 2/lib/main.dart
@@ -4,14 +4,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'input_cubit.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,20 +23,15 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String word = '';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // backgroundColor: Colors.white,
         body: Container(
           margin: EdgeInsets.only(top: 200),
           padding: EdgeInsets.all(24.0),
@@ -56,7 +46,6 @@ class _MyHomePageState extends State<MyHomePage> {
                           TextFormField(
                             decoration: InputDecoration(
                               hintText: 'User Input : ',
-                              // fillColor: Colors.grey[100],
                               filled: true,
                               suffixIcon: IconButton(
                                 onPressed: () {
